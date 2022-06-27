8326102f63d8b1a23bcc46f20da9e3bdccb95a45
ivancity <shieldm@hotmail.com>
2022-06-27 16:25:12 +0300

    adding dependency injection using Dagger Hilt

---
+++ app/src/main/java/com/ivan/m/democalorietracker/DemoCalorieTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.ivan.m.democalorietracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DemoCalorieTrackerApp: Application()
---
+++ app/src/main/java/com/ivan/m/democalorietracker/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.ivan.m.democalorietracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.ivan.m.core.domain.data.preferences.DefaultPreferences
+import com.ivan.m.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+}
