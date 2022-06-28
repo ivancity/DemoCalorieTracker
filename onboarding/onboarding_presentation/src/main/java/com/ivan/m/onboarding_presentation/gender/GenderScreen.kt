cc32449fa42f1e44e42e6965a43741ee5af25d54
ivancity <shieldm@hotmail.com>
2022-06-28 08:12:53 +0300

    implementing the GenderScreen

--- onboarding/onboarding_presentation/src/main/java/com/ivan/m/onboarding_presentation/gender/GenderScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/ivan/m/onboarding_presentation/gender/GenderScreen.kt
@@ -1,14 +1,22 @@
 package com.ivan.m.onboarding_presentation.gender
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ivan.m.core.util.UiEvent
 import com.ivan.m.core_ui.LocalSpacing
+import com.ivan.m.core.R
+import com.ivan.m.core.domain.model.Gender
+import com.ivan.m.onboarding_presentation.components.ActionButton
+import com.ivan.m.onboarding_presentation.components.SelectableButton
 
 @Composable
 fun GenderScreen(
@@ -29,7 +37,49 @@ fun GenderScreen(
             .fillMaxSize()
             .padding(spacing.spaceLarge)
     ) {
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.whats_your_gender),
+                style = MaterialTheme.typography.h3
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            Row() {
+                SelectableButton(
+                    text = stringResource(id = R.string.male),
+                    isSelected = viewModel.selectedGender is Gender.Male,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGenderClick(Gender.Male)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.female),
+                    isSelected = viewModel.selectedGender is Gender.Female,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGenderClick(Gender.Female)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+            }
+        }
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = viewModel::onNextClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
     }
 
 }
