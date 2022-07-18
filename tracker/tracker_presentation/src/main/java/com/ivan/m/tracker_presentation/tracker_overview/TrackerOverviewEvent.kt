5064a1e156e2645c9231f208b359629b7ab78b28
ivancity <shieldm@hotmail.com>
2022-07-18 11:39:44 -0500

    viewmodel implementation. adding to preferences more functions to save and load boolean for Show Onboarding. Adding some drawables for the UI. Adding a Meal model to use for the UI, it is similar to FoodTracked in the Domain module but this is model has certain properties exclusively for UI. Adding a TrackerOverviewEvent for events that happen on the UI. Adding a TrackerOverviewState for displaying the actual data on screen. The viewModel has a Job in order to cancel in an easy way Flows that we instantiate.

---
+++ tracker/tracker_presentation/src/main/java/com/ivan/m/tracker_presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package com.ivan.m.tracker_presentation.tracker_overview
+
+import com.ivan.m.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDateClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): TrackerOverviewEvent()
+}
