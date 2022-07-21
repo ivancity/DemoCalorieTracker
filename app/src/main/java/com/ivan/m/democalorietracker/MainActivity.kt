24dc8dcab149c32774caf6a5d57eeecc3ad94ef8
ivancity <shieldm@hotmail.com>
2022-07-21 11:01:36 -0500

    Fixes: Main activity now knows which screen to display first. SearchScreen now hides the keyboard after an action is complete, also it shows the text hint when needed. TrackableFoodItem now has a number keyboard. Overview always tries to refresh from the network every time it opens the TrackerOverview screen.

--- app/src/main/java/com/ivan/m/democalorietracker/MainActivity.kt
+++ app/src/main/java/com/ivan/m/democalorietracker/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
+import com.ivan.m.core.domain.preferences.Preferences
 import com.ivan.m.core.navigation.Route
 import com.ivan.m.democalorietracker.navigation.navigate
 import com.ivan.m.democalorietracker.ui.theme.DemoCalorieTrackerTheme
@@ -30,13 +31,19 @@ import com.ivan.m.onboarding_presentation.welcome.WelcomeScreen
 import com.ivan.m.tracker_presentation.search.SearchScreen
 import com.ivan.m.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @ExperimentalComposeUiApi
 @ExperimentalCoilApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
         setContent {
             DemoCalorieTrackerTheme {
                 val navController = rememberNavController()
@@ -47,7 +54,9 @@ class MainActivity : ComponentActivity() {
                 ) { _ ->
                     NavHost(
                         navController = navController,
-                        startDestination = Route.WELCOME
+                        startDestination = if(shouldShowOnboarding) {
+                            Route.WELCOME
+                        } else Route.TRACKER_OVERVIEW
                     ) {
                         composable(Route.WELCOME) {
                             WelcomeScreen(onNavigate = navController::navigate)
